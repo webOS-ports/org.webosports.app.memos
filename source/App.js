bc938bcd285fe4cb23e5bbc0d79f752dd4a972fa
josh <jpalmerwatkins@me.com>
2013-01-12 01:09:56 +0000

    org.webosports.app.memos: Add webos-ports-lib CoreNavi support
    * Add CoreNavi
    * Add finger-tracking CoreNavi support
    
    Open-WebOS-DCO-1.0-Signed-Off-By: Josh Palmer <jpalmerwatkins@me.com>

--- source/App.js
+++ source/App.js
@@ -98,14 +98,14 @@ var storageObject = {};
 var searchResults = {};
 
 enyo.kind({
-	name: "App",
+	name: "AppPanels",
 	kind: "Panels",
 	fit: true,
 	realtimeFit: true,
 	arrangerKind: "CollapsingArranger",
 	draggable: false,
+	classes: "app-panels",
 	components:[
-		{kind: "BackGesture", onBack: "handleBackGesture"},
 		{name: "MenuPanel",
 		layoutKind: "FittableRowsLayout",
 		style: "width: 33%",
@@ -175,19 +175,6 @@ enyo.kind({
 	rendered: function(inSender) {
 		this.inherited(arguments);
 		this.loadMemos();
-	},
-	reflow: function(inSender) {
-		this.inherited(arguments);
-		if(enyo.Panels.isScreenNarrow()) {
-			this.setArrangerKind("PushPopArranger");
-			this.setDraggable(false);
-			this.$.ContentPanels.addStyles("box-shadow: 0");
-		}
-		else {
-			this.setArrangerKind("CollapsingArranger");
-			this.setDraggable(true);
-			this.$.ContentPanels.addStyles("box-shadow: -4px 0px 4px rgba(0,0,0,0.3)");
-		}
 	},
 	handleBackGesture: function(inSender, inEvent) {
 		return this.setIndex(0);
@@ -359,5 +346,53 @@ enyo.kind({
 			}
 		}
 		r.setCount(m);
+	}
+});
+
+enyo.kind({
+	name: "App",
+	layoutKind: "FittableRowsLayout",
+	components: [
+		{kind: "Signals",
+		ondeviceready: "deviceready",
+		onbackbutton: "handleBackGesture",
+		onCoreNaviDragStart: "handleCoreNaviDragStart",
+		onCoreNaviDrag: "handleCoreNaviDrag",
+		onCoreNaviDragFinish: "handleCoreNaviDragFinish",},
+		{name: "AppPanels", kind: "AppPanels", fit: true},
+		{kind: "CoreNavi", fingerTracking: true}
+	],
+	//Handlers
+	reflow: function(inSender) {
+		this.inherited(arguments);
+		if(enyo.Panels.isScreenNarrow()) {
+			this.$.AppPanels.setArrangerKind("CoreNaviArranger");
+			this.$.AppPanels.setDraggable(false);
+			this.$.AppPanels.$ContentPanels.addStyles("box-shadow: 0");
+		}
+		else {
+			this.$.AppPanels.setArrangerKind("CollapsingArranger");
+			this.$.AppPanels.setDraggable(true);
+			this.$.AppPanels.$.ContentPanels.addStyles("box-shadow: -4px 0px 4px rgba(0,0,0,0.3)");
+		}
+	},
+	handleBackGesture: function(inSender, inEvent) {
+		this.$.AppPanels.setIndex(0);
+	},
+	handleCoreNaviDragStart: function(inSender, inEvent) {
+		this.$.AppPanels.dragstartTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+	handleCoreNaviDrag: function(inSender, inEvent) {
+		this.$.AppPanels.dragTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+	handleCoreNaviDragFinish: function(inSender, inEvent) {
+		this.$.AppPanels.dragfinishTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+	//Utility Functions
+	reverseDrag: function(inEvent) {
+		inEvent.dx = -inEvent.dx;
+		inEvent.ddx = -inEvent.ddx;
+		inEvent.xDirection = -inEvent.xDirection;
+		return inEvent;
 	}
 });
