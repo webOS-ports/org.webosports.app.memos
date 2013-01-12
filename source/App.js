af439d19d17c97ecd96c2a07ed4ac5084d4786e4
josh <jpalmerwatkins@me.com>
2013-01-12 04:27:19 +0000

    Local Storage: Save correctly on memo change
    Open-WebOS-DCO-1.0-Signed-Off-By: Josh Palmer <jpalmerwatkins@me.com>

--- source/App.js
+++ source/App.js
@@ -53,9 +53,9 @@ enyo.kind({
 			kind: "RichText",
 			content: "",
 			style: "position: absolute; top: 0; bottom: 0; left: 0; right: 0; padding: 20px 32px;",
+			oninput: "memoChanged",
 			handlers: {
-				ondragstart: "",
-				oninput: "memoChanged"
+				ondragstart: ""
 			}},
 		]},
 		{kind: "onyx.Toolbar", components:[
