5037517b45d778d9e8bbd276d7a8b21d6cda477e
john.microtech@gmail.com <john.microtech@gmail.com>
2015-01-19 13:11:51 -0500

    add/ move to view/style/data folders
    Open-WebOS-DCO-1.0-Signed-Off-By: john mcconnell  johnmcconnell@yahoo.com

---
+++ source/views/memos.js
@@ -0,0 +1,56 @@
+var storageObject = [];
+var searchResults = {};
+
+enyo.kind({
+	name: "memos.MainView",
+	layoutKind: "FittableRowsLayout",
+	components: [
+		{kind: "Signals",
+		ondeviceready: "deviceready",
+		onbackbutton: "handleBackGesture",
+		onCoreNaviDragStart: "handleCoreNaviDragStart",
+		onCoreNaviDrag: "handleCoreNaviDrag",
+		onCoreNaviDragFinish: "handleCoreNaviDragFinish",},
+		{name: "AppPanels", kind: "AppPanels", fit: true},
+		{kind: "CoreNavi", fingerTracking: true}
+	],
+
+	//Handlers
+	reflow: function(inSender) {
+		this.inherited(arguments);
+		if(enyo.Panels.isScreenNarrow()) {
+			this.$.AppPanels.setArrangerKind("CoreNaviArranger");
+			this.$.AppPanels.setDraggable(false);
+			this.$.AppPanels.$.ContentPanels.addStyles("box-shadow: 0");
+		}
+		else {
+			this.$.AppPanels.setArrangerKind("CollapsingArranger");
+			this.$.AppPanels.setDraggable(true);
+			this.$.AppPanels.$.ContentPanels.addStyles("box-shadow: -4px 0px 4px rgba(0,0,0,0.3)");
+		}
+	},
+
+	handleBackGesture: function(inSender, inEvent) {
+		this.$.AppPanels.setIndex(0);
+	},
+
+	handleCoreNaviDragStart: function(inSender, inEvent) {
+		this.$.AppPanels.dragstartTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+
+	handleCoreNaviDrag: function(inSender, inEvent) {
+		this.$.AppPanels.dragTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+
+	handleCoreNaviDragFinish: function(inSender, inEvent) {
+		this.$.AppPanels.dragfinishTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+
+	//Utility Functions
+	reverseDrag: function(inEvent) {
+		inEvent.dx = -inEvent.dx;
+		inEvent.ddx = -inEvent.ddx;
+		inEvent.xDirection = -inEvent.xDirection;
+		return inEvent;
+	}
+});
